21fea9c9b2eabac0aec0d741d8c6882cd2927a5f
Arthur Alves Correa <arthur.correa@ibm.com>
2022-09-06 19:20:24 -0300

    fix: fix part of cmp changes

--- lua/personal_cmp.lua
+++ lua/personal_cmp.lua
@@ -1,6 +1,10 @@
 local cmp = require "cmp"
 
 cmp.setup {
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  },
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
@@ -17,20 +21,32 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<c-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<c-y>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "vsnip" }, -- For vsnip users.
-    { name = "luasnip" }, -- For luasnip users.
     { name = "ultisnips" }, -- For ultisnips users.
     { name = "snippy" }, -- For snippy users.
   }, {
-    { name = "buffer" },
+    { name = "buffer", keyword_length = 5 },
   }),
 }
+
+cmp.setup.filetype("lua", {
+  { name = "nvim_lsp" },
+  { name = "vsnip" }, -- For vsnip users.
+  { name = "luasnip" }, -- For luasnip users.
+  { name = "ultisnips" }, -- For ultisnips users.
+  { name = "snippy" }, -- For snippy users.
+}, {
+  { name = "buffer", keyword_length = 5 },
+})
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
